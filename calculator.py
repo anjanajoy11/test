7d740eba1c76889776886a36215106ce31a4f27a
anjanajoy11 <anjanajoy2004@gmail.com>
2026-04-22 13:33:10 +0530

    calaculator

---
+++ calculator.py
@@ -0,0 +1,9 @@
+num1=float(input("Enter first number: "))
+num2=float(input("Enter second number: "))
+operator=input("The operator from +,-,*:")
+if operator == "+":
+    print(num1+num2)    
+elif operator == "-":
+    print(num1-num2)
+elif operator == "*":
+    print(num1*num2)    
